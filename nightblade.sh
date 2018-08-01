f62accfb2d73c0c67a32f1b6e105270e2cc826b0
Paolo Perego <thesp0nge@gmail.com>
2018-08-01 18:26:53 +0200

    Adding background

--- nightblade.sh
+++ nightblade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Inspired by
+# Inspired and code borrowed by
 #  g0tmilk ~ https://blog.g0tmi1k.com/
 #  drkpasngr ~ https://drkpasngr.github.io/
 #  https://github.com/sourcekris/kali-postinstall/blob/master/kali-postinstall.sh
@@ -19,17 +19,6 @@ TOTAL=$( grep '(${STAGE}/${TOTAL})' $0 | wc -l );(( TOTAL-- ))  # How many thing
 
 
 timezone="Europe/Rome"
-if [[ -n "${timezone}" && ! -f "/usr/share/zoneinfo/${timezone}" ]]; then
-  echo -e ' '${RED}'[!]'${RESET}" Looks like the ${RED}timezone '${timezone}'${RESET} is incorrect/not supported (Example: ${BOLD}Europe/London${RESET})" 1>&2
-  echo -e ' '${RED}'[!]'${RESET}" Quitting..." 1>&2
-  exit 1
-elif [[ -n "${keyboardLayout}" && -e /usr/share/X11/xkb/rules/xorg.lst ]]; then
-  if ! $(grep -q " ${keyboardLayout} " /usr/share/X11/xkb/rules/xorg.lst); then
-    echo -e ' '${RED}'[!]'${RESET}" Looks like the ${RED}keyboard layout '${keyboardLayout}'${RESET} is incorrect/not supported (Example: ${BOLD}gb${RESET})" 1>&2
-    echo -e ' '${RED}'[!]'${RESET}" Quitting..." 1>&2
-    exit 1
-  fi
-fi
 
 ##### Check if we are running as root - else this script will fail (Hard!)
 if [[ "${EUID}" -ne 0 ]]; then
@@ -37,7 +26,7 @@ if [[ "${EUID}" -ne 0 ]]; then
   echo -e ' '${RED}'[!]'${RESET}" Quitting..." 1>&2
   exit 1
 else
-  echo -e " ${BLUE}[*]${RESET} ${BOLD}Post-installation script for Kali Linux.${RESET}"
+  echo -e " ${BLUE}[*]${RESET} ${BOLD}Nighblade post-installation script for Kali Linux.${RESET}"
   sleep 3s
 fi
 
@@ -45,6 +34,325 @@ fi
 export DISPLAY=:0.0
 export TERM=xterm
 
-(( STAGE++ )); echo -e "${GREEN}[+] ${RESET} (${STAGE}/${TOTAL}) Setting hostname to nightblade"
+(( STAGE++ )); echo -e " ${GREEN}[+] ${RESET} (${STAGE}/${TOTAL}) Setting hostname to nightblade"
 sed -i 's/kali/nightblade/g' /etc/hosts
 sed -i 's/kali/nightblade/g' /etc/hostname
+
+##### Are we using GNOME?
+if [[ $(which gnome-shell) ]]; then
+  ##### RAM check
+  if [[ "$(free -m | grep -i Mem | awk '{print $2}')" < 2048 ]]; then
+    echo -e '\n '${RED}'[!]'${RESET}" ${RED}You have <= 2GB of RAM and using GNOME${RESET}" 1>&2
+    echo -e " ${YELLOW}[i]${RESET} ${YELLOW}Might want to use XFCE instead${RESET}..."
+    sleep 15s
+  fi
+
+
+  ##### Disable its auto notification package updater
+  (( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Disabling GNOME's ${GREEN}notification package updater${RESET} service ~ in case it runs during this script"
+  export DISPLAY=:0.0
+  timeout 5 killall -w /usr/lib/apt/methods/http >/dev/null 2>&1
+
+
+  ##### Disable screensaver
+  (( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Disabling ${GREEN}screensaver${RESET}"
+  xset s 0 0
+  xset s off
+  gsettings set org.gnome.desktop.session idle-delay 0
+else
+  echo -e "\n\n ${YELLOW}[i]${RESET} ${YELLOW}no gnome-shell installed. A gnome working environment was expected${RESET}..."
+  exit 1
+fi
+
+##### Check Internet access
+(( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Checking ${GREEN}Internet access${RESET}"
+#--- Can we ping google?
+for i in {1..10}; do ping -c 1 -W ${i} www.google.com &>/dev/null && break; done
+#--- Run this, if we can't
+if [[ "$?" -ne 0 ]]; then
+  echo -e ' '${RED}'[!]'${RESET}" ${RED}Possible DNS issues${RESET}(?)" 1>&2
+  echo -e ' '${RED}'[!]'${RESET}" Will try and use ${YELLOW}DHCP${RESET} to 'fix' the issue" 1>&2
+  chattr -i /etc/resolv.conf 2>/dev/null
+  dhclient -r
+  #--- Second interface causing issues?
+  ip addr show eth1 &>/dev/null
+  [[ "$?" == 0 ]] \
+    && route delete default gw 192.168.155.1 2>/dev/null
+  #--- Request a new IP
+  dhclient
+  dhclient eth0 2>/dev/null
+  dhclient wlan0 2>/dev/null
+  #--- Wait and see what happens
+  sleep 15s
+  _TMP="true"
+  _CMD="$(ping -c 1 8.8.8.8 &>/dev/null)"
+  if [[ "$?" -ne 0 && "$_TMP" == "true" ]]; then
+    _TMP="false"
+    echo -e ' '${RED}'[!]'${RESET}" ${RED}No Internet access${RESET}" 1>&2
+    echo -e ' '${RED}'[!]'${RESET}" You will need to manually fix the issue, before re-running this script" 1>&2
+  fi
+  _CMD="$(ping -c 1 www.google.com &>/dev/null)"
+  if [[ "$?" -ne 0 && "$_TMP" == "true" ]]; then
+    _TMP="false"
+    echo -e ' '${RED}'[!]'${RESET}" ${RED}Possible DNS issues${RESET}(?)" 1>&2
+    echo -e ' '${RED}'[!]'${RESET}" You will need to manually fix the issue, before re-running this script" 1>&2
+  fi
+  if [[ "$_TMP" == "false" ]]; then
+    (dmidecode | grep -iq virtual) && echo -e " ${YELLOW}[i]${RESET} VM Detected"
+    (dmidecode | grep -iq virtual) && echo -e " ${YELLOW}[i]${RESET} ${YELLOW}Try switching network adapter mode${RESET} (e.g. NAT/Bridged)"
+    echo -e ' '${RED}'[!]'${RESET}" Quitting..." 1>&2
+    exit 1
+  fi
+else
+  echo -e " ${YELLOW}[i]${RESET} ${YELLOW}Detected Internet access${RESET}" 1>&2
+fi
+
+#--- GitHub under DDoS?
+(( STAGE++ )); echo -e " ${GREEN}[i]${RESET} (${STAGE}/${TOTAL}) Checking ${GREEN}GitHub status${RESET}"
+timeout 300 curl --progress -k -L -f "https://status.github.com/api/status.json" | grep -q "good" \
+  || (echo -e ' '${RED}'[!]'${RESET}" ${RED}GitHub is currently having issues${RESET}. ${BOLD}Lots may fail${RESET}. See: https://status.github.com/" 1>&2 \
+    && exit 1)
+
+
+##### Enable default network repositories ~ http://docs.kali.org/general-use/kali-linux-sources-list-repositories
+(( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Enabling default OS ${GREEN}network repositories${RESET}"
+#--- Add network repositories
+file=/etc/apt/sources.list; [ -e "${file}" ] && cp -n $file{,.bkup}
+([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+#--- Main
+grep -q '^deb .* kali-rolling' "${file}" 2>/dev/null \
+  || echo -e "\n\n# Kali Rolling\ndeb http://http.kali.org/kali kali-rolling main contrib non-free" >> "${file}"
+#--- Source
+grep -q '^deb-src .* kali-rolling' "${file}" 2>/dev/null \
+  || echo -e "deb-src http://http.kali.org/kali kali-rolling main contrib non-free" >> "${file}"
+#--- Disable CD repositories
+sed -i '/kali/ s/^\( \|\t\|\)deb cdrom/#deb cdrom/g' "${file}"
+#--- incase we were interrupted
+dpkg --configure -a
+#--- Update
+apt -qq update
+if [[ "$?" -ne 0 ]]; then
+  echo -e ' '${RED}'[!]'${RESET}" There was an ${RED}issue accessing network repositories${RESET}" 1>&2
+  echo -e " ${YELLOW}[i]${RESET} Are the remote network repositories ${YELLOW}currently being sync'd${RESET}?"
+  echo -e " ${YELLOW}[i]${RESET} Here is ${BOLD}YOUR${RESET} local network ${BOLD}repository${RESET} information (Geo-IP based):\n"
+  curl -sI http://http.kali.org/README
+  exit 1
+fi
+
+##### Check to see if Kali is in a VM. If so, install "Virtual Machine Addons/Tools" for a "better" virtual experiment
+if (dmidecode | grep -iq vmware); then
+  ##### Install virtual machines tools ~ http://docs.kali.org/general-use/install-vmware-tools-kali-guest
+  (( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}VMware's (open) virtual machine tools${RESET}"
+  apt -y -qq install open-vm-tools-desktop fuse \
+    || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+  apt -y -qq install make \
+    || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2    # There's a nags afterwards
+  ## Shared folders support for Open-VM-Tools (some odd bug)
+  file=/usr/local/sbin/mount-shared-folders; [ -e "${file}" ] && cp -n $file{,.bkup}
+  cat <<EOF > "${file}" \
+    || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
+#!/bin/bash
+
+vmware-hgfsclient | while read folder; do
+  echo "[i] Mounting \${folder}   (/mnt/hgfs/\${folder})"
+  mkdir -p "/mnt/hgfs/\${folder}"
+  umount -f "/mnt/hgfs/\${folder}" 2>/dev/null
+  vmhgfs-fuse -o allow_other -o auto_unmount ".host:/\${folder}" "/mnt/hgfs/\${folder}"
+done
+
+sleep 2s
+EOF
+  chmod +x "${file}"
+  ln -sf "${file}" /root/Desktop/mount-shared-folders.sh
+elif (dmidecode | grep -iq virtualbox); then
+  ##### Installing VirtualBox Guest Additions.   Note: Need VirtualBox 4.2.xx+ for the host (http://docs.kali.org/general-use/kali-linux-virtual-box-guest)
+  (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}VirtualBox's guest additions${RESET}"
+  apt -y -qq install virtualbox-guest-x11 \
+    || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+fi
+
+(( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Setting Italian Keyboard layout"
+cat <<EOF > /etc/default/keyboard
+XKBLAYOUT=it,us
+BACKSPACE=guess
+XKBVARIANT=,
+EOF
+
+if [[ -n "${timezone}" ]]; then
+  (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Updating ${GREEN}location information${RESET} ~ time zone (${BOLD}${timezone}${RESET})"
+  echo "${timezone}" > /etc/timezone
+  ln -sf "/usr/share/zoneinfo/$(cat /etc/timezone)" /etc/localtime
+  dpkg-reconfigure -f noninteractive tzdata
+else
+  echo -e "\n\n ${YELLOW}[i]${RESET} ${YELLOW}Skipping time zone${RESET} (missing: '$0 ${BOLD}--timezone <value>${RESET}')..." 1>&2
+fi
+
+#--- Installing ntp tools
+(( STAGE++ )); echo -e " ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}ntpdate${RESET} ~ keeping the time in sync"
+apt -y -qq install ntp ntpdate \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+#--- Update time
+ntpdate -b -s -u pool.ntp.org
+#--- Start service
+systemctl restart ntp
+#--- Remove from start up
+systemctl disable ntp 2>/dev/null
+#--- Only used for stats at the end
+start_time=$(date +%s)
+
+##### Update OS from network repositories
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) ${GREEN}Updating OS${RESET} from network repositories"
+echo -e " ${YELLOW}[i]${RESET}  ...this ${BOLD}may take a while${RESET} depending on your Internet connection & Kali version/age"
+for FILE in clean autoremove; do apt -y -qq "${FILE}"; done         # Clean up      clean remove autoremove autoclean
+export DEBIAN_FRONTEND=noninteractive
+apt -qq update && APT_LISTCHANGES_FRONTEND=none apt -o Dpkg::Options::="--force-confnew" -y dist-upgrade --fix-missing 2>&1 \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+#--- Cleaning up temp stuff
+for FILE in clean autoremove; do apt -y -qq "${FILE}"; done         # Clean up - clean remove autoremove autoclean
+#--- Check kernel stuff
+_TMP=$(dpkg -l | grep linux-image- | grep -vc meta)
+if [[ "${_TMP}" -gt 1 ]]; then
+  echo -e "\n ${YELLOW}[i]${RESET} Detected ${YELLOW}multiple kernels${RESET}"
+  TMP=$(dpkg -l | grep linux-image | grep -v meta | sort -t '.' -k 2 -g | tail -n 1 | grep "$(uname -r)")
+  if [[ -z "${TMP}" ]]; then
+    echo -e '\n '${RED}'[!]'${RESET}' You are '${RED}'not using the latest kernel'${RESET} 1>&2
+    echo -e " ${YELLOW}[i]${RESET} You have it ${YELLOW}downloaded${RESET} & installed, just ${YELLOW}not USING IT${RESET}"
+    #echo -e "\n ${YELLOW}[i]${RESET} You ${YELLOW}NEED to REBOOT${RESET}, before re-running this script"
+    #exit 1
+    sleep 3s
+  else
+    echo -e " ${YELLOW}[i]${RESET} ${YELLOW}You're using the latest kernel${RESET} (Good to continue)"
+  fi
+fi
+
+##### Install kernel headers
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}kernel headers${RESET}"
+apt -y -qq install make gcc "linux-headers-$(uname -r)" \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+if [[ $? -ne 0 ]]; then
+  echo -e ' '${RED}'[!]'${RESET}" There was an ${RED}issue installing kernel headers${RESET}" 1>&2
+  echo -e " ${YELLOW}[i]${RESET} Are you ${YELLOW}USING${RESET} the ${YELLOW}latest kernel${RESET}?"
+  echo -e " ${YELLOW}[i]${RESET} ${YELLOW}Reboot${RESET} your machine"
+  #exit 1
+  sleep 30s
+fi
+
+
+##### Install "kali full" meta packages (default tool selection)
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}kali-linux-full${RESET} meta-package"
+echo -e " ${YELLOW}[i]${RESET}  ...this ${BOLD}may take a while${RESET} depending on your Kali version (e.g. ARM, light, mini or docker...)"
+#--- Kali's default tools ~ https://www.kali.org/news/kali-linux-metapackages/
+apt -y -qq install kali-linux-full \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+
+
+##### Set audio level
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Setting ${GREEN}audio${RESET} levels"
+systemctl --user enable pulseaudio
+systemctl --user start pulseaudio
+pactl set-sink-mute 0 0
+pactl set-sink-volume 0 25%
+
+if [[ $(dmidecode | grep -i virtual) ]]; then
+  ###### Configure login screen
+  (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Configuring ${GREEN}login screen${RESET}"
+  #--- Enable auto (gui) login
+  file=/etc/gdm3/daemon.conf; [ -e "${file}" ] && cp -n $file{,.bkup}
+  sed -i 's/^.*AutomaticLoginEnable = .*/AutomaticLoginEnable = true/' "${file}"
+  sed -i 's/^.*AutomaticLogin = .*/AutomaticLogin = root/' "${file}"
+fi
+
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}fonts${RESET}"
+apt -y -qq install fonts-inconsolata fonts-firacode \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+
+
+
+if [[ $(which gnome-shell) ]]; then
+  ##### Configure GNOME 3
+  (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Configuring ${GREEN}GNOME 3${RESET} ~ desktop environment"
+  export DISPLAY=:0.0
+  #-- Gnome Extension - Dash Dock (the toolbar with all the icons)
+  gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true      # Set dock to use the full height
+  gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'LEFT'   # Set dock to the right
+  gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false         # Set dock to be always visible
+  gsettings set org.gnome.shell favorite-apps \
+    "['gnome-terminal.desktop', 'org.gnome.Nautilus.desktop', 'firefox-esr.desktop', 'kali-burpsuite.desktop', 'sublime-text.desktop', 'gedit.desktop', 'pycharm.desktop', 'gnome-screenshot.desktop']"
+  #-- Gnome Extension - Alternate-tab (So it doesn't group the same windows up)
+  GNOME_EXTENSIONS=$(gsettings get org.gnome.shell enabled-extensions | sed 's_^.\(.*\).$_\1_')
+  echo "${GNOME_EXTENSIONS}" | grep -q "alternate-tab@gnome-shell-extensions.gcampax.github.com" \
+    || gsettings set org.gnome.shell enabled-extensions "[${GNOME_EXTENSIONS}, 'alternate-tab@gnome-shell-extensions.gcampax.github.com']"
+  #-- Gnome Extension - Drive Menu (Show USB devices in tray)
+  GNOME_EXTENSIONS=$(gsettings get org.gnome.shell enabled-extensions | sed 's_^.\(.*\).$_\1_')
+  echo "${GNOME_EXTENSIONS}" | grep -q "drive-menu@gnome-shell-extensions.gcampax.github.com" \
+    || gsettings set org.gnome.shell enabled-extensions "[${GNOME_EXTENSIONS}, 'drive-menu@gnome-shell-extensions.gcampax.github.com']"
+  #--- Workspaces
+  gsettings set org.gnome.shell.overrides dynamic-workspaces true                          # Static
+  gsettings set org.gnome.desktop.wm.preferences num-workspaces 3                          # Increase workspaces count to 3
+  #--- Top bar
+  gsettings set org.gnome.desktop.interface clock-show-date true                           # Show date next to time in the top tool bar
+  gsettings set org.gnome.desktop.interface clock-show-seconds true                        # Show seconds next to time in the top tool bar
+
+
+  gsettings set org.gnome.desktop.interface font-name "Inconsolata Medium 10"
+  gsettings set org.gnome.desktop.wm.preferences titlebar-font "Inconsolata Medium 12"
+  gsettings set org.gnome.desktop.background show-desktop-icons false
+  #--- Keyboard short-cuts
+  (dmidecode | grep -iq virtual) && gsettings set org.gnome.mutter overlay-key "Alt_R"   # Change 'super' key to right side (rather than left key), if in a VM
+  #--- Hide desktop icon
+  dconf write /org/gnome/nautilus/desktop/computer-icon-visible true
+else
+  echo -e "\n\n ${YELLOW}[i]${RESET} ${YELLOW}Skipping GNOME${RESET}..." 1>&2
+fi
+
+##### Configure file   Note: need to restart xserver for effect
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Configuring ${GREEN}file${RESET} (Nautilus/Thunar) ~ GUI file system navigation"
+#--- Settings
+mkdir -p ~/.config/gtk-2.0/
+file=~/.config/gtk-2.0/gtkfilechooser.ini; [ -e "${file}" ] && cp -n $file{,.bkup}
+([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+sed -i 's/^.*ShowHidden.*/ShowHidden=false/' "${file}" 2>/dev/null \
+  || cat <<EOF > "${file}"
+[Filechooser Settings]
+LocationMode=path-bar
+ShowHidden=false
+ExpandFolders=true
+ShowSizeColumn=true
+GeometryX=66
+GeometryY=39
+GeometryWidth=780
+GeometryHeight=618
+SortColumn=name
+SortOrder=ascending
+EOF
+dconf write /org/gnome/nautilus/preferences/show-hidden-files false
+#--- Bookmarks
+file=/root/.gtk-bookmarks; [ -e "${file}" ] && cp -n $file{,.bkup}
+([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+mkdir -p /X
+grep -q '^file:///usr/share/exploitdb ' "${file}" 2>/dev/null \
+  || echo 'file:///usr/share/exploitdb/ exploit-db' >> "${file}"
+grep -q '^file:///opt ' "${file}" 2>/dev/null \
+  || echo 'file:///opt Tools' >> "${file}"
+grep -q '^file:///usr/share ' "${file}" 2>/dev/null \
+  || echo 'file:///usr/share Kali Tools' >> "${file}"
+grep -q '^file:///var/www/html ' "${file}" 2>/dev/null \
+  || echo 'file:///var/www/html www' >> "${file}"
+  (dmidecode | grep -iq vmware) \
+  && (mkdir -p /mnt/hgfs/ 2>/dev/null; grep -q '^file:///mnt/hgfs ' "${file}" 2>/dev/null \
+    || echo 'file:///mnt/hgfs VMShare' >> "${file}")
+grep -q '^file:///tmp ' "${file}" 2>/dev/null \
+  || echo 'file:///tmp /tmp' >> "${file}"
+
+
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Cloning ${GREEN}QOGIR GTK/GNOME-SHELL theme${RESET}"
+rm -rf ~/Qogir-theme
+rm -rf /usr/share/themes/Qogir*
+git clone https://github.com/vinceliuice/Qogir-theme.git
+~/Qogir-theme/install.sh
+gsettings set org.gnome.desktop.interface gtk-theme "Qogir-win-light"
+gsettings set org.gnome.shell.extensions.user-theme name "Qogir-win-light"
+
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Install ${GREEN}mouse theme${RESET}"
+apt install breeze-cursor-theme
+gsettings set org.gnome.desktop.interface cursor-theme "Breeze_Snow"
